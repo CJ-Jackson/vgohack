aa75e225a7557cc6456c282bb067534043e3d9c8
Christopher Jackson <hello@cj-jackson.com>
2018-08-18 11:24:54 +0100

    Updated vgohack

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/CJ-Jackson/vgohack
--- vgohack.go
+++ vgohack.go
@@ -1,99 +1,43 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"log"
 	"os"
 	"os/exec"
-
-	"github.com/CJ-Jackson/vgohack/listjson"
-	"github.com/CJ-Jackson/vgohack/modjson"
 )
 
-const replace = "}\n{"
-
-const with = "},\n{"
-
 func main() {
-	if len(os.Args) <= 2 {
-		other()
-	}
-	if os.Args[1] == "mod" && os.Args[2] == "-json" {
-		useListInstead()
-	}
-	other()
-}
-
-func useListInstead() {
-	buf := &bytes.Buffer{}
-	cmd := exec.Command("vgo", "list", "-m", "-json", "all")
-	cmd.Stdin = cmd.Stdin
-	cmd.Stderr = cmd.Stderr
-	cmd.Stdout = buf
-	if nil != cmd.Run() {
-		os.Exit(1)
+	if len(os.Args) <= 2 || os.Args[1] != "mod" {
+		cmd := exec.Command("go", os.Args[1:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+		return
 	}
 
-	b := buf.Bytes()
-	buf.Reset()
-
-	b = bytes.Replace(b, []byte(replace), []byte(with), -1)
-	buf.Write([]byte("["))
-	buf.Write(b)
-	buf.Write([]byte("]"))
-
-	listJ := []listjson.Module{}
-	if err := json.NewDecoder(buf).Decode(&listJ); nil != err {
-		log.Fatal(err)
-	}
-	buf.Reset()
-
-	goMod := &modjson.GoMod{}
-
-	for _, j := range listJ {
-		if j.Main {
-			goMod.Module = modjson.Module{
-				Path:    j.Path,
-				Version: j.Version,
-			}
-			continue
-		}
-
-		goMod.Require = append(goMod.Require, modjson.Module{
-			Path:    j.Path,
-			Version: j.Version,
-		})
-
-		if nil != j.Replace {
-			goMod.Replace = append(goMod.Replace, modjson.Replace{
-				Old: modjson.Module{
-					Path:    j.Path,
-					Version: j.Version,
-				},
-				New: modjson.Module{
-					Path:    j.Replace.Path,
-					Version: j.Replace.Version,
-				},
-			})
-		}
+	modFlagMap := map[string]string{
+		"-require": "edit",
+		"-fix":     "fix",
+		"-graph":   "graph",
+		"-init":    "init",
+		"-sync":    "tidy",
+		"-vendor":  "vendor",
+		"-verify":  "verify",
 	}
 
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("\t", "\t")
-	if nil != enc.Encode(*goMod) {
-		os.Exit(1)
+	arg2 := os.Args[2]
+	if arg2[0] == '-' {
+		arg2 = modFlagMap[arg2]
 	}
-	os.Exit(0)
-}
 
-func other() {
-	cmd := exec.Command("vgo", os.Args[1:]...)
-	cmd.Stderr = os.Stderr
+	args := []string{os.Args[1], arg2}
+	if len(os.Args) > 3 {
+		args = append(args, os.Args[3:]...)
+	}
+
+	cmd := exec.Command("go", args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
-	if nil != cmd.Run() {
-		os.Exit(1)
-	}
-	os.Exit(0)
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
